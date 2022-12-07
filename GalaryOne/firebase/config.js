7fb4af37ae33498ee21d45677283b037daaf843e
abuzafarghaffari <76465986+abuzafarghaffari@users.noreply.github.com>
2022-12-07 14:46:28 +0530

    Update config.js

--- GalaryOne/firebase/config.js
+++ GalaryOne/firebase/config.js
@@ -7,12 +7,9 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDGXpWj8rKE2GF6ITOPROaJconDk2IEu-U",
-  authDomain: "firegram-93fd0.firebaseapp.com",
-  projectId: "firegram-93fd0",
-  storageBucket: "firegram-93fd0.appspot.com",
-  messagingSenderId: "987528343193",
-  appId: "1:987528343193:web:7b9a28265b6f9609f99208"
+  xxxxxx
+  xxxxxx
+  xxxxxxx
 };
 
 // Initialize Firebase
